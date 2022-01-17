87c9b9039d0ac194f5156a9bed82f4293951c73f
Erica S. Francisco <ericasantosfrancisco@gmail.com>
2022-01-17 20:22:24 -0300

    JWT e TOKEN para Login

--- routes/users.js
+++ routes/users.js
@@ -2,81 +2,182 @@ const express = require('express');
 const router = express.Router();
 const mysql2 = require('../mysql2').pool;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+//CADASTRO DO USUARIO
+router.post('/cadastro', (req, res, next) => {
+    mysql2.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error}) }
+        conn.query('SELECT * FROM user WHERE email = ?', [req.body.email], (error, results) => {
+            if (error){ return res.status(500).send({error: error}) }
+            if (results.length > 0) {
+                res.status(409).send({ mensagem: 'Usuário já cadastrado'})
+            } else {
+                bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+                    if(errBcrypt) { return res.status(500).send({ error: errBcrypt})}
+                    conn.query(
+                        `INSERT INTO user (nome, sobrenome, email, senha) VALUES (?, ?, ?, ?)`, 
+                        [req.body.nome, req.body.sobrenome, req.body.email, hash],
+                         (error, results) => {
+                             conn.release(); //para liberar a conexão
+                             if (error){
+                                 return res.status(500).send({error: error}) }
+                                response = {
+                                    mensagem: 'usuário criado com sucesso',
+                                    usuarioCriado: {
+                                       id_user: results.insertId,
+                                       nome: req.body.nome,
+                                       sobrenome: req.body.sobrenome,
+                                       email: req.body.email
+                                    }
+                                }
+                                return res.status(201).send(response)
+                         })
+                }) 
+            };
+        })
+
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    mysql2.getConnection((error, conn) => {
+            if (error){ return res.status(500).send({error: error}) }
+            const query = `SELECT * FROM user WHERE email = ?`;
+            conn.query(query, [req.body.email],(error, results, fields) => {
+                conn.release();
+                if (error){ return res.status(500).send({error: error}) }
+                if (results.length < 1) {
+                    return res.status(401).send({ mensagem: 'Falha na altentificação'})
+                }
+
+                bcrypt.compare(req.body.senha, results[0].senha, (error, results) => {
+                    if (error){
+                        return res.status(401).send({ mensagem: 'Falha na altentificação'})
+                    }
+                    if (results){
+                        const token = jwt.sign({
+                            id_user: results[0].id_user,
+                            email: results[0].email
+                        }, 
+                        process.env.JWT_KEY, 
+                        {
+                            expiresIn: "1h" //depois de uma hora o user terá que refazer o login
+                        });
+
+                        return res.status(200).send({ 
+                            mensagem: 'Autenticado com sucesso',
+                            token: token
+                        });
+                    }
+                    return res.status(401).send({ mensagem: 'Falha na altentificação'})
+                });
+            });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //RETORNA TODOS OS USUÁRIOS
-router.get('/', (req, res, next) =>{
-    res.status(200).send({
-        mensagem: "Usando o GET dentro de users"
-    });
-});
-
-//INSERE UM USUÁRIO
-router.post('/:add', (req, res, next) => {
-
-//      const usuario = {
-//      id_user: req.body.id_user,
-//      nome: req.body.nome,
-//      sobrenome: req.body.sobrenome,
-//      email: req.body.email,
-//      senha: req.body.senha
-//   };
-
-    mysql2.getConnection((err, conn) => {
+router.get('/:cadastrados', (req, res, next) =>{
+  
+    mysql2.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({error: error}) }
         
+        conn.query(
+            'SELECT * FROM user;',
+            (error, results, field) => {
                 if (error) {
                     return res.status(500).send({error: error}) }
-                
-                 bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-                     if (errBcrypt) { return res.status(500).send({error: errBcrypt}) }
-
-                     conn.query(
-                        'INSERT INTO users (id_user, nome, sobrenome, email, senha) VALUES (?, ?)',
-                        [req.body.id_user, req.body.nome, req.body.sobrenome, req.body.email, req.body.hash],
-                        (error, results) => {
-                             conn.release(); //Importante: quando ele chegar no call back, essa linha vai liberar a conexão para não sobrecarregar a conexão
-            
-                            res.status(201).send({
-                                mensagem: "Dados inseridos com sucesso",
-                                id_user: resultado.insertId
-                        });
-
-                });
-             }
+                    res.status(201).send({response: results});
+            }
         )
-    });
-
+    })
+    
 });
 
 // RETORNA OS DADOS DE UM USUÁRIO
-router.get('/:id_usuario', (req, res, next) =>{
-    const id = req.params.id_usuario
-
-    if (id === 'especial'){
-        res.status(200).send({
-            mensagem: "Você achou o ID especial",
-            id:id
-        });
-    } else {
-        res.status(200).send({
-            mensagem: "Você achou o ID comum.",
-            id:id
-        });
-    }
+router.get('/:id_user', (req, res, next) =>{
+    mysql2.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error}) }
+        
+        conn.query(
+            'SELECT * FROM user WHERE id_user = ?;',
+            [req.params.id_user],
+            (error, results, fields) => {
+                if (error) {return res.status(500).send({error: error}) }
+                return res.status(200).send({response: results})
+            }
+        )
+    });
 });
 
 // ALTERA OS DADOS DO USUÁRIO
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Usando o PATCH dentro da rota de users"
-    });
+router.patch('/editar', (req, res, next) => {
+    mysql2.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({error: error}) }
+
+        conn.query(
+            `UPDATE user
+                SET   nome        = ?,
+                      sobrenome   = ?,
+                      email       = ?,
+                      senha       = ?
+                WHERE id_user     = ?`,
+        [req.body.nome, req.body.sobrenome, req.body.email, req.body.senha, req.body.id_user],
+        (error, results) => {
+            conn.release();
+            if (error) {
+                return res.status(500).send({error: error}); } 
+            
+            res.status(202).send({
+                mensagem: "Dados alterados com sucesso"
+            });
+        }
+        )
+    })
 });
 
 // EXCLUI OS DADOS
-router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Usando o DELETE dentro da rota de users"
-    });
+router.delete('/deletar', (req, res, next) => {
+    mysql2.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({error: error}) }
+
+        conn.query(
+            'DELETE FROM user WHERE id_user = ?', [req.body.id_user],
+        (error, results, field) => {
+            conn.release();
+            if (error) {
+                return res.status(500).send({error: error}); } 
+            
+            res.status(202).send({
+                mensagem: "Usuário removido com sucesso"
+            });
+        }
+        )
+    })
 });
 
 
